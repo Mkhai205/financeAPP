3cbb661d7330b4fbfc618b73fae2c20ae3a0048b
Mkhai205 <156907229+Mkhai205@users.noreply.github.com>
2024-11-11 20:02:11 +0700

    Update controller.py

--- controller.py
+++ controller.py
@@ -1,3 +1,6 @@
+from model import PremiumAccount, BasicAccount
+from model import Transaction
+
 class FinanceController:
     # Hàm khởi tạo Controller
     def __init__(self, model, view):
@@ -8,6 +11,7 @@ class FinanceController:
         # Kết nối các nút với các hàm xử lý
         self._view.btn_register.config(command=self.register_user)
         self._view.btn_login.config(command=self.login_user)
+        self._view.btn_register_premium.config(command=self.register_premium)
 
         self._view.btn_logout.config(command=self.logout_user)
         self._view.btn_add.config(command=self.add_transaction)
@@ -33,7 +37,7 @@ class FinanceController:
 
     # Hàm cập nhật báo cáo
     def update_report(self):
-        transactions = self._model.get_transactions_db()
+        transactions = self._model.get_transactions()
         total_income = 0
         total_expense = 0
         for trans in transactions:
@@ -42,31 +46,55 @@ class FinanceController:
             else:
                 total_expense += trans[1]
                 
-        budget = self._model.get_user_budget()
-        balance = self._model.get_user_balance()
+        budget = self._model.get_budget()
+        balance = self._model.get_balance()
         
-        if total_expense > budget:
+        if self._model.get_account_type == "premium" and total_expense > budget:
             self._view.display_message("Cảnh báo", "Chi tiêu đã vượt quá ngân sách!")
 
         self._view.clear_data_input()
         self._view.update_transactions_display(transactions)
         self._view.update_financial_info(total_income, total_expense, balance, budget)
 
+    def verify(self, username, password, account_type):
+        self._model.set_account_info(username, password, account_type)
+        if self._model.register():
+            self._view.clear_login_info()
+            if account_type == "premium":    
+                self._view.display_message("Thành công", "Đăng ký thành công tài khoản Premium!")
+            else:
+                self._view.display_message("Thành công", "Đăng ký thành công!")
+        else:
+            self._view.display_error("Lỗi", "Tên đăng nhập đã tồn tại!")
+        
+    
     # Hàm đăng ký người dùng
     def register_user(self):
         username, password = self._view.get_login_info()
-        if self._model.register_user(username, password):
-            self._view.clear_login_info()
-            self._view.display_message("Thành công", "Đăng ký thành công!")
-        else:
-            self._view.display_error("Lỗi", "Tên đăng nhập đã tồn tại!")
+        account_type = "basic"
+        self.verify(username, password, account_type)
+        
 
+    def register_premium(self):
+        username, password = self._view.get_login_info()
+        account_type = "premium"
+        self.verify(username, password, account_type)
+    
     # Hàm đăng nhập người dùng
     def login_user(self):
         username, password = self._view.get_login_info()
-        if self._model.verify_user(username, password):
+        self._model.set_account_info(username, password)
+        if self._model.login():
             self._view.show_application_view()
             self._view.set_username(username)
+            if self._model.get_account_type() == "premium":
+                self._view.show_premium_features()
+                self._model = PremiumAccount(username=username, password=password, account_type="premium")
+            else:
+               self._view.hide_premium_features() 
+               self._model = BasicAccount(username=username, password=password, account_type="basic")
+            
+            self._model.update_account_info()
             self.update_report()
             self._view.display_message("Đăng cập thành công \n", f"Chào mừng {username} đến với ứng dụng quản lý tài chính!")
         else:
@@ -74,7 +102,7 @@ class FinanceController:
     
     # Hàm đăng xuất người dùng
     def logout_user(self):
-        self._model.logout_user()
+        self._model.logout()
         self._view.show_login_view()
         self._view.clear_login_info()
     
@@ -84,7 +112,7 @@ class FinanceController:
         if not self.validate_amount_category(data[0], data[2]):
             return
         
-        if not self._model.add_transaction_db(data):
+        if not self._model.add_transaction(data):
             print(f"Error: {data}")
             self._view.display_error("Lỗi", "Có lỗi xảy ra khi thêm giao dịch!")
             return
@@ -99,8 +127,7 @@ class FinanceController:
             self._view.display_error("Lỗi", "Vui lòng chọn một giao dịch để xóa!")
             return
         
-        trans_id = selected_item[0]
-        self._model.delete_transaction_db(trans_id)
+        self._model.delete_transaction(selected_item)
         self.update_report()
         self._view.display_message("Thành công", "Giao dịch đã được xóa!")
 
@@ -108,7 +135,7 @@ class FinanceController:
     def set_budget(self):
         try:
             budget = int(self._view.get_budget_input())
-            self._model.set_user_budget(budget)
+            self._model.set_budget(budget)
             self.update_report()
             self._view.display_message("Thành công", f"Ngân sách đã được đặt thành {budget}")
         except ValueError:
@@ -128,14 +155,13 @@ class FinanceController:
             self._view.display_error("Lỗi", "Vui lòng chọn một giao dịch để sửa!")
             return
         
-        trans_id = selected_item[0]
         data = self._view.get_data_input()
         
         if not self.validate_amount_category(data[0], data[2]):
             return
         
         try:
-            self._model.update_transaction_db(trans_id, data)
+            self._model.update_transaction(selected_item, data)
             self.update_report()
             self._view.display_message("Thành công", "Giao dịch đã được sửa!")
         except Exception as e:
@@ -150,6 +176,6 @@ class FinanceController:
         income_data = self._model.get_total_income_by_category()
         
         # Lấy ngân sách
-        budget = self._model.get_user_budget()
+        budget = self._model.get_budget()
         
         self._view.draw_chart(expense_data, income_data, budget)
